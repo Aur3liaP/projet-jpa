30d48c320f766486deb147ea46c8f959dc3e867f
Pic Aurélia <pic.aurelia@gmail.com>
2025-05-13 17:33:59 +0200

    Add JavaDoc

--- src/main/java/fr/diginamic/entities/Role.java
+++ src/main/java/fr/diginamic/entities/Role.java
@@ -2,55 +2,112 @@ package fr.diginamic.entities;
 
 import jakarta.persistence.*;
 
+/**
+ * Représente un rôle joué par un acteur dans un film.
+ * Cette classe est une entité JPA avec une clé composite composée de {@link Film} et {@link Acteur}.
+ */
 @Entity
 @IdClass(RoleId.class)
 public class Role {
 
+    /**
+     * Le film associé à ce rôle.
+     */
     @Id
     @ManyToOne
     @JoinColumn(name = "ID_FILM")
     private Film film;
 
+    /**
+     * L'acteur associé à ce rôle.
+     */
     @Id
     @ManyToOne
     @JoinColumn(name = "ID_ACTEUR")
     private Acteur acteur;
 
+    /**
+     * Le nom du personnage joué par l'acteur dans le film.
+     */
     private String personnage;
 
+    /**
+     * Constructeur par défaut requis par JPA.
+     */
     public Role() {}
 
+    /**
+     * Constructeur pour créer un rôle avec un film, un acteur et un personnage spécifiés.
+     *
+     * @param film       Le film associé à ce rôle.
+     * @param acteur     L'acteur associé à ce rôle.
+     * @param personnage Le nom du personnage joué par l'acteur.
+     */
     public Role(Film film, Acteur acteur, String personnage) {
         this.film = film;
         this.acteur = acteur;
         this.personnage = personnage;
     }
 
-
+    /**
+     * Obtient le film associé à ce rôle.
+     *
+     * @return Le film associé.
+     */
     public Film getFilm() {
         return film;
     }
 
+    /**
+     * Définit le film associé à ce rôle.
+     *
+     * @param film Le film à associer.
+     */
     public void setFilm(Film film) {
         this.film = film;
     }
 
+    /**
+     * Obtient l'acteur associé à ce rôle.
+     *
+     * @return L'acteur associé.
+     */
     public Acteur getActeur() {
         return acteur;
     }
 
+    /**
+     * Définit l'acteur associé à ce rôle.
+     *
+     * @param acteur L'acteur à associer.
+     */
     public void setActeur(Acteur acteur) {
         this.acteur = acteur;
     }
 
+    /**
+     * Obtient le nom du personnage joué par l'acteur.
+     *
+     * @return Le nom du personnage.
+     */
     public String getPersonnage() {
         return personnage;
     }
 
+    /**
+     * Définit le nom du personnage joué par l'acteur.
+     *
+     * @param personnage Le nom du personnage à définir.
+     */
     public void setPersonnage(String personnage) {
         this.personnage = personnage;
     }
 
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de ce rôle.
+     *
+     * @return Une chaîne de caractères représentant ce rôle.
+     */
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Role{");
@@ -61,4 +118,3 @@ public class Role {
         return sb.toString();
     }
 }
-
--- src/main/java/fr/diginamic/entities/RoleId.java
+++ src/main/java/fr/diginamic/entities/RoleId.java
@@ -3,17 +3,45 @@ package fr.diginamic.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Classe représentant une clé composite pour l'entité {@link Role}.
+ * Cette classe est utilisée comme identifiant composite pour les rôles joués par des acteurs dans des films.
+ * Elle implémente {@link Serializable} pour permettre la sérialisation.
+ */
 public class RoleId implements Serializable {
+
+    /**
+     * L'identifiant du film associé à ce rôle.
+     */
     private String film;
+
+    /**
+     * L'identifiant de l'acteur associé à ce rôle.
+     */
     private String acteur;
 
+    /**
+     * Constructeur par défaut requis pour JPA.
+     */
     public RoleId() {}
 
+    /**
+     * Constructeur pour créer une clé composite avec un film et un acteur spécifiés.
+     *
+     * @param film   L'identifiant du film.
+     * @param acteur L'identifiant de l'acteur.
+     */
     public RoleId(String film, String acteur) {
         this.film = film;
         this.acteur = acteur;
     }
 
+    /**
+     * Compare cet objet à un autre pour l'égalité.
+     *
+     * @param o L'objet à comparer.
+     * @return true si les objets sont égaux, false sinon.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -23,6 +51,11 @@ public class RoleId implements Serializable {
                 Objects.equals(acteur, that.acteur);
     }
 
+    /**
+     * Génère un code de hachage pour cet objet.
+     *
+     * @return Le code de hachage.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(film, acteur);
